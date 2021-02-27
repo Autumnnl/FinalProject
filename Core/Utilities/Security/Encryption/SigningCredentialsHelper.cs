b3cf8b54ae265d1d0dea03adf0dd6cc1828d438e
Bahar <bahar_vipelamu@hotmail.com>
2021-02-27 23:48:07 +0300

    27

--- Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace Core.Utilities.Security.Encryption
 {
-    public class SigningHelper
+    public class SigningCredentialsHelper
     {
-        public static SigningCredentials CreateSigningcredentials(SecurityKey security)
+        public static SigningCredentials CreateSigningCredentials(SecurityKey security)
         {
             return new SigningCredentials(security,SecurityAlgorithms.HmacSha512Signature);
 
