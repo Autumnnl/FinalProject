688736ef86b546d6850dab678f6ce12df16a08eb
Bahar <bahar_vipelamu@hotmail.com>
2021-03-04 00:13:32 +0300

    303

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,7 +3,9 @@ using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Caching;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
 using Core.Utilities.Results;
@@ -24,7 +26,7 @@ namespace Business.Concrete
 
         IProductDal _productDal;
         ICategoryService _categoryService;
-
+        
  
         public ProductManager(IProductDal productDal,ICategoryService categoryService)
         {
@@ -35,6 +37,7 @@ namespace Business.Concrete
 
         [SecuredOperation("admin,product.add")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
             #region BusinessRules
@@ -55,6 +58,7 @@ namespace Business.Concrete
         [CacheAspect] ////key,value
         public IDataResult<List<Product>> GetAll()
         {
+
             if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenaceTime);
@@ -67,7 +71,7 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
         }
-
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p=> p.ProductId == productId));
@@ -83,6 +87,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             throw new NotImplementedException();
@@ -117,6 +122,16 @@ namespace Business.Concrete
 
             return new SuccessResult();
         }
-    
+        //[TransactionAspect]
+        public IResult AddTransactionalTest(Product product)
+        {
+            Add(product);
+            if (product.UnitPrice < 10)
+            {
+                throw new Exception("");
+            }
+            Add(product);
+            return null;
+        }
     }
 }
